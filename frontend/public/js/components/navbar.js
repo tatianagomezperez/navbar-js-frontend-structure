9b1f80a65d92ff520fc07056bba68b9df514603e
Tatiana Gomez <yeimitatianagomezperez@gmail.com>
2025-07-02 18:55:30 -0500

    feat: se agregoresponsividad al navbar y se creo el archivo navbar.js

---
+++ frontend/public/js/components/navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const navbarElement = document.querySelector(".navbar-container");
+
+    if(navbarElement){
+        fetch("/frontend/public/views/components/navbar.html")
+            .then(response => response.text())
+            .then(data => {
+                navbarElement.innerHTML = data;
+            })
+            .catch(error => console.error("Error cargando el navbar", error));
+    }
+});
